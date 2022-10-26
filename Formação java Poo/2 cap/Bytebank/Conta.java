c272f7f15c88627a690ec4eaedc509d9cf0061bf
Lucas Henrique Calheiros Saraiva Santos <43302698+lucashenriquecss@users.noreply.github.com>
2022-10-26 15:28:35 -0300

    A

--- Formação java Poo/2 cap/Bytebank/Conta.java
+++ Formação java Poo/2 cap/Bytebank/Conta.java
@@ -3,4 +3,29 @@ public class Conta {
     double saldo;
     int agencia,numero;
     String titular;
+    String cpf;
+    String profissao;
+    //METODOS
+    public void deposita(double valor){
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor){
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public boolean transferir(double valor,Conta destino){
+        if (this.saldo >= valor) {
+            this.saldo -=valor;
+            destino.deposita(valor);
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
